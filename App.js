0cbbcdd93db9bbe9eb37bd55960859e9bb148cee
Gelya <gelya.ervine@student.umn.ac.id>
2025-04-14 12:54:48 +0700

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import Home from "./tabs/Home.tsx";
+import Notifications from "./tabs/Notifications.tsx";
+import You from "./tabs/You.tsx";
+import { PaperProvider } from "react-native-paper";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === "Home") {
+                iconName = focused ? "home" : "home-outline";
+              } else if (route.name === "Notifications") {
+                iconName = focused ? "notifications" : "notifications-outline";
+              } else if (route.name === "You") {
+                iconName = focused ? "person" : "person-outline";
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Notifications" component={Notifications} />
+          <Tab.Screen name="You" component={You} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
